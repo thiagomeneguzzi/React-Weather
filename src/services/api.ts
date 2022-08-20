6ff234c79f0bf92d3a15e89db21733a81d0b66cb
thiagomeneguzzi <thiagonmeneguzzi@gmail.com>
2022-08-20 10:24:04 -0300

    fix env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const key = process.env.REACT_APP_API_KEY
+const key = import.meta.env.VITE_API_KEY
 
 export function getWeather(city: string) {
   return axios.get(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&aqi=no&lang=pt`)
