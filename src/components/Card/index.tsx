b0c6afb8dddf010187330925827ce3da99da60e5
thiagomeneguzzi <thiagonmeneguzzi@gmail.com>
2022-08-20 14:01:07 -0300

    added new icons

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,9 +1,9 @@
 import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 
-import { BsSun, BsCloudy } from 'react-icons/bs';
-import { MdNightlightRound } from 'react-icons/md';
+import { BsMoonStars } from 'react-icons/bs';
 import { TbError404 } from 'react-icons/tb';
+import { WiCloud, WiCloudy, WiDaySunny, WiFog, WiRain, WiRainMix, WiShowers, WiSleet, WiSnow, WiSnowWind, WiStormShowers, WiThunderstorm } from 'react-icons/wi';
 
 interface Props {
   temperature: number;
@@ -15,9 +15,55 @@ interface Props {
 }
 
 const conditions = [
-  {'Sol': <BsSun />},
-  {'Céu limpo': <MdNightlightRound />},
-  {'Parcialmente nublado': <BsCloudy />}
+  {'Sol': <WiDaySunny />},
+  {'Céu limpo': <BsMoonStars />},
+  {'Parcialmente nublado': <WiCloud />},
+  {'Nublado': <WiCloudy />},
+  {'Encoberto': <WiFog />},
+  {'Neblina': <WiFog />},
+  {'Nevoeiro': <WiFog />},
+  {'Nevoeiro gelado': <WiFog />},
+  {'Possibilidade de chuva irregular': <WiCloudy />},
+  {'Possibilidade de neve irregular': <WiCloudy />},
+  {'Possibilidade de neve molhada irregular': <WiCloudy />},
+  {'Possibilidade de chuvisco gelado irregular': <WiCloudy />},
+  {'Possibilidade de trovoada': <WiCloudy />},
+  {'Rajadas de vento com neve': <WiSnowWind />},
+  {'Nevasca': <WiSnow />},
+  {'Chuvisco irregular': <WiShowers />},
+  {'Chuvisco': <WiShowers />},
+  {'Chuvisco': <WiShowers />},
+  {'Chuvisco forte gelado': <WiSleet />},
+  {'Chuva fraca irregular': <WiSleet />},
+  {'Chuva fraca': <WiSleet />},
+  {'Períodos de chuva moderada': <WiSleet />},
+  {'Chuva moderada': <WiSleet />},
+  {'Períodos de chuva forte': <WiRain />},
+  {'Chuva forte': <WiRain />},
+  {'Chuva fraca e gelada': <WiShowers />},
+  {'Chuva gelada moderada ou forte': <WiRain />},
+  {'Chuva fraca com neve': <WiRainMix />},
+  {'Chuva forte ou moderada com neve': <WiRainMix />},
+  {'Queda de neve irregular e fraca': <WiSnow />},
+  {'Queda de neve fraca': <WiSnow />},
+  {'Queda de neve moderada e irregular': <WiSnow />},
+  {'Queda de neve moderada': <WiSnow />},
+  {'Queda de neve forte e irregular': <WiSnow />},
+  {'Neve intensa': <WiSnow />},
+  {'Granizo': <WiRainMix />},
+  {'Aguaceiros fracos': <WiShowers />},
+  {'Aguaceiros moderados ou fortes': <WiShowers />},
+  {'Chuva torrencial': <WiRain />},
+  {'Aguaceiros fracos com neve': <WiSnow />},
+  {'Aguaceiros moderados ou fortes com neve': <WiRainMix />},
+  {'Chuva fraca com neve': <WiSnow />},
+  {'Chuva moderada ou forte com neve': <WiRainMix />},
+  {'Aguaceiros fracos com granizo': <WiRainMix />},
+  {'Aguaceiros moderados ou fortes com granizo': <WiRainMix />},
+  {'Chuva fraca irregular com trovoada': <WiStormShowers />},
+  {'Chuva moderada ou forte com trovoada': <WiThunderstorm />},
+  {'Neve fraca irregular com trovoada': <WiSnow />},
+  {'Neve moderada ou forte com trovoada': <WiSnow />},
 ]
 
 export function Card({temperature, condition, feelsLike, localtime, city, state}: Props) {
